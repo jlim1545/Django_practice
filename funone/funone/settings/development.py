e832afd43db3c29157ed2f262b83d6772cf21662
Josh <joshualim.1@gmail.com>
2017-03-26 23:45:04 -0700

    mysql setup

--- funone/funone/settings/development.py
+++ funone/funone/settings/development.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 from .base import *
 DEBUG=True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
